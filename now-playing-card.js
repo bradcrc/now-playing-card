d373ab8731da327bf258e6bf660c0d3211c75b4d
Zambu <jose@aurestic.es>
2020-10-20 13:17:12 +0200

    [ADD] 'on' state for LG WebOS tv

--- now-playing-card.js
+++ now-playing-card.js
@@ -1,89 +1,89 @@
 class NowPlayingPoster extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
-      const card = document.createElement('ha-card');  
-      this.content = document.createElement('div');	
-	  
-	  
+      const card = document.createElement('ha-card');
+      this.content = document.createElement('div');
+
+
 	  //this.content.style = "!important;";
-	  
-	  
+
+
       card.appendChild(this.content);
 	  card.style = "background: none;";
       this.appendChild(card);
-	  
-	  
+
+
     }
 
-	const offposter = this.config.off_image;		
+	const offposter = this.config.off_image;
     const entityId = this.config.entity;
-	const state = hass.states[entityId];	
+	const state = hass.states[entityId];
 	const stateStr = state ? state.state : 'unavailable';
-	
-	
-	
-	if (state) {		
-	
-		const movposter = state.attributes.entity_picture;	
-	
-		if (stateStr == "playing") {
-			if ( !movposter ) {			
-				if ( offposter ) {				
+
+
+
+	if (state) {
+
+		const movposter = state.attributes.entity_picture;
+
+		if (["playing", "on"].indexOf(stateStr) > -1 ) {
+			if ( !movposter ) {
+				if ( offposter ) {
 					this.content.innerHTML = `
 					<!-- now playing card ${entityId} -->
 					<img src="${offposter}" width=100% align="center" style="">
-					`;						
-				} 		
-				else			
-				{		
-					this.content.innerHTML = `		
-					<!-- now playing card ${entityId}  no image-->			
-					`;				
+					`;
 				}
-			} 	
-			else		
-			{
-			this.content.innerHTML = `		
-			<!-- now playing card ${entityId}  -->
-			<img src="${movposter}" width=100% height=100%">	  
-			`;			
+				else
+				{
+					this.content.innerHTML = `
+					<!-- now playing card ${entityId}  no image-->
+					`;
+				}
 			}
-		} 	
-		else		
-		{		
-			
-			if ( offposter ) {				
+			else
+			{
+			this.content.innerHTML = `
+			<!-- now playing card ${entityId}  -->
+			<img src="${movposter}" width=100% height=100%">
+			`;
+			}
+		}
+		else
+		{
+
+			if ( offposter ) {
 				this.content.innerHTML = `
 				<!-- now playing card ${entityId} -->
 				<img src="${offposter}" width=100% align="center" style="">
-				`;						
-			} 		
-			else			
-			{		
-				this.content.innerHTML = `		
-				<!-- now playing card ${entityId}  no image-->			
-				`;				
-			}			
-		
+				`;
+			}
+			else
+			{
+				this.content.innerHTML = `
+				<!-- now playing card ${entityId}  no image-->
+				`;
+			}
+
 		}
 
-  
-	} 	
-	else		
-	{
-	
-		
-    this.content.innerHTML = `		
-	<!-- now playing card ${entityId} not playing --> 
-    `;
-	
+
 	}
-	
+	else
+	{
+
+
+    this.content.innerHTML = `
+	<!-- now playing card ${entityId} not playing -->
+    `;
+
+	}
+
   }
-  
-  
- 
-  
+
+
+
+
   setConfig(config) {
     if (!config.entity) {
       throw new Error('You need to define an entity');
@@ -91,7 +91,7 @@ class NowPlayingPoster extends HTMLElement {
     this.config = config;
   }
 
-  
+
   // The height of your card. Home Assistant uses this to automatically
   // distribute all cards over the available columns.
   getCardSize() {
